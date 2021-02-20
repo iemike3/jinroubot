9b95e482d18041f0572edbeaf9b3058629db9c3c
iemike3 <69967018+iemike3@users.noreply.github.com>
2021-02-20 13:33:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 
-import discord,json
+import discord,json,datetime
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="j_")
 
 with open("config.json","r") as f:
     config = json.load(f)
+
+@bot.event
+async def on_ready():
+    print("---------------------")
+    print(f"User:{bot.user}")
+    print(f"Login Time:{datetime.datetime.now()}")
 
 bot.run(config["token"])
